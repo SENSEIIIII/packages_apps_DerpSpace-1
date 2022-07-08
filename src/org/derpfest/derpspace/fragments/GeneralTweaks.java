2761d856b4a0ce06270705fc9be85c08b9f9fbe2
minarypenguin <alexfinhart@gmail.com>
2022-07-08 07:50:20 +0000

    Custom Dashboard styles
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

--- src/org/derpfest/derpspace/fragments/GeneralTweaks.java
+++ src/org/derpfest/derpspace/fragments/GeneralTweaks.java
@@ -39,11 +39,15 @@ public class GeneralTweaks extends SettingsPreferenceFragment implements OnPrefe
 
 
     private static final String ALERT_SLIDER_PREF = "alert_slider_notifications";
-    private static final String KEY_SPOOF = "use_photos_spoof";
-    private static final String SYS_SPOOF = "persist.sys.pixelprops.gphotos";
+    private static final String KEY_GAMES_SPOOF = "use_games_spoof";
+    private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
+
+    private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
+    private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
 
     private Preference mAlertSlider;
-    private SwitchPreference mSpoof;
+    private SwitchPreference mGamesSpoof;
+    private SwitchPreference mPhotosSpoof;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -61,9 +65,13 @@ public class GeneralTweaks extends SettingsPreferenceFragment implements OnPrefe
         if (!mAlertSliderAvailable)
             prefScreen.removePreference(mAlertSlider);
 
-        mSpoof = (SwitchPreference) findPreference(KEY_SPOOF);
-        mSpoof.setChecked(SystemProperties.getBoolean(SYS_SPOOF, true));
-        mSpoof.setOnPreferenceChangeListener(this);
+        mGamesSpoof = (SwitchPreference) prefScreen.findPreference(KEY_GAMES_SPOOF);
+        mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
+        mGamesSpoof.setOnPreferenceChangeListener(this);
+
+        mPhotosSpoof = (SwitchPreference) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
+        mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
+        mPhotosSpoof.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -77,13 +85,14 @@ public class GeneralTweaks extends SettingsPreferenceFragment implements OnPrefe
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mSpoof) {
-            String value = ((Boolean) objValue) ? "1" : "0";
-            SystemProperties.set(SYS_SPOOF, value);
-            Toast.makeText(getActivity(),
-                    (R.string.photos_spoof_toast),
-                    Toast.LENGTH_LONG).show();
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mGamesSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
+            return true;
+        } else if (preference == mPhotosSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
             return true;
         }
         return false;
